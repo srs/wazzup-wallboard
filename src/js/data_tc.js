22f0962c7281a0d4282869db23dd3711bf4e1725
Runar Myklebust <rmy@enonic.com>
2013-03-08 14:08:53 +0100

    Added data fetching for TC

--- src/js/data_tc.js
+++ src/js/data_tc.js
@@ -17,24 +17,56 @@ app.data.tc = {
         return result;
     },
 
+    loadAll: function () {
+        this.loadBuildTypes();
+        this.loadAgents();
+        //$(window).trigger('loadData');
+    },
+
     loadBuildTypes: function () {
         var buildTypes = this.getJson('/app/rest/buildTypes');
+
         this.loadBuilds(buildTypes);
     },
 
     loadBuilds: function (projects) {
+
+        app.data.model.tc.builds.broken = [];
+
+        app.data.model.tc.builds.count = projects.buildType.length;
+
         for (var i = 0; i < projects.buildType.length; i++) {
+
             var buildInfo = this.getJson('/app/rest/builds/?locator=buildType:' + projects.buildType[i].id + '&count=1');
-            this.loadBuildDetails(buildInfo, i);
+
+            var buildStatus = buildInfo.build[0];
+
+            if (buildStatus.status != "SUCCESS") {
+                console.log("Build success, for id: " + buildStatus.id);
+                this.loadBrokenBuilds(buildInfo);
+            }
         }
     },
 
-    loadBuildDetails: function (buildInfo, index) {
+    loadBrokenBuilds: function (buildInfo) {
         var buildDetails = this.getJson('/app/rest/builds/id:' + buildInfo.build[0].id);
-        var buildStats = this.getJson('/app/rest/builds/id:' + buildInfo.build[0].id + '/statistics');
 
-        app.data.model.tc.builds[index] = buildDetails;
-        app.data.model.tc.builds[index].stats = buildStats;
+        var relatedIssues = buildDetails.relatedIssues;
+
+        if (relatedIssues != null) {
+
+            var changeId = relatedIssues.issueUsage[0].changes.change[0].id;
+            var changeDetails = this.getJson('/app/rest/changes/id:' + changeId);
+
+
+            var brokenBuildInfo = {
+                id: changeDetails.username,
+                change: changeDetails.comment,
+                date: changeDetails.date
+            }
+
+            app.data.model.tc.builds.broken.push(brokenBuildInfo);
+        }
     },
 
     loadAgents: function () {
@@ -42,19 +74,35 @@ app.data.tc = {
 
         console.log(agentInfos);
 
+        var agentCount = agentInfos.agent.length;
+        var agentsRunning = 0;
+        var agentsFailure = 0;
+
         for (var i = 0; i < agentInfos.agent.length; i++) {
             var agentRunning = this.getJson("/app/rest/agents/id:" + agentInfos.agent[i].id + "/enabled");
-            app.data.model.tc.agents[i] = {
-                "name": agentInfos.agent[i].name,
-                "running": agentRunning
-            };
-        }
-    },
 
-    loadAll: function () {
-        this.loadBuildTypes();
-        this.loadAgents();
-        $(window).trigger('loadData');
+            if (agentRunning) {
+                agentsRunning++;
+            } else {
+                agentsFailure++;
+            }
+        }
+
+        app.data.model.tc.agents = {
+            count: agentCount,
+            running: agentsRunning,
+            failures: agentsFailure
+        }
+
     }
-};
+}
+;
+
+$(document).ready(function () {
+    app.data.tc.loadAll();
+
+    setInterval(function () {
+        app.data.tc.loadAll();
+    }, app.config.tc.interval);
+});
 
